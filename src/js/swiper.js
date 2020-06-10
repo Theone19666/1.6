d4728df0df929fd96015b109411c122fe31e8b1b
Nelly <nelli.pavlova.19@gmail.com>
2020-06-10 23:30:19 +0300

    добавила два предпоследних блока

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,17 +1,22 @@
 import Swiper from "swiper";
 
-function createSwiper() {
-  return new Swiper(".swiper-container", {
+function createSwiper(selector = ".swiper-container", width = 240) {
+  return new Swiper(selector, {
     pagination: {
       el: ".swiper-pagination",
       type: "bullets",
     },
     speed: 400,
     //spaceBetween: 16,
-    width: 240,
+    width,
   });
 }
 
-if (window.innerWidth < 768 && document.querySelector(".swiper-container")) {
-  createSwiper();
+if (window.innerWidth < 768) {
+  if (document.querySelector(".swiper-container")) {
+    createSwiper(".swiper-container:not(.prices-swiper-container)");
+  }
+  if (document.querySelector(".prices-swiper-container")) {
+    createSwiper(".prices-swiper-container", 260);
+  }
 }
